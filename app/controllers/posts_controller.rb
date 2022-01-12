05d6f027920b5879417e6f5f4d2a0578cdbdb7a2
chiharu05 <2102ct@gmail.com>
2022-01-12 17:52:25 +0900

    一覧表示機能のビューを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,42 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to past_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to root_path
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:date_and_time, :kind_id, :memo)
+  end
 end
